408bfcd4cacd65ec57a7bb500ecee6992a40a6d4
Imgroot22 <yogesh.r3@cognizant.com>
2022-06-15 18:04:25 +0530

    added loading to TweetsByLoginId comp

--- src/components/TweetsByLoginId/TweetsByLoginId.js
+++ src/components/TweetsByLoginId/TweetsByLoginId.js
@@ -3,21 +3,26 @@ import { useParams } from "react-router-dom";
 import { getTweetsByLoginId } from "../../services/tweet-service";
 import { useAuth } from "../../store/auth-context";
 import Tweets from "./Tweets/Tweets";
+import ReactLoading from "react-loading";
 
 const TweetsByLoginId = () => {
   const param = useParams();
   const auth = useAuth();
   const [tweets, setTweets] = useState([]);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    setIsLoading(true);
     getTweetsByLoginId(
       auth.token,
       param.loginId,
       (data) => {
         setTweets(data);
+        setIsLoading(false);
       },
       (err) => {
         setError(err.data);
+        setIsLoading(false);
       }
     );
   }, [param.loginId, auth.token]);
@@ -26,7 +31,7 @@ const TweetsByLoginId = () => {
     setTweets((prev) => prev.filter((x) => x.id !== tweet.id));
   };
 
-  return (
+  return !isLoading ? (
     <div className="mt-3">
       {error ? (
         error
@@ -35,6 +40,16 @@ const TweetsByLoginId = () => {
       ) : (
         <Tweets tweets={tweets} editEnabled={false} />
       )}
+    </div>
+  ) : (
+    <div className="d-flex w-100">
+      <ReactLoading
+        type="balls"
+        color="rgba(33,37,41,1)"
+        height="120px"
+        width="120px"
+        className="m-auto"
+      />
     </div>
   );
 };
